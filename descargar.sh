24ad98f83a9d5db650b497b7717d394b0d77d95c
cattaino <dantecattaino@gmail.com>
2023-08-07 22:22:38 +0000

    Script 'descargar.sh' - 3rd approach

--- descargar.sh
+++ descargar.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+URL="$1"
+SUM="$2"
+HTPS="https://*"
 if [[ ! $URL =~ $HTPS || ! $SUM =~ $HTPS || ! $# -eq 2 ]]; then
 echo "Datos ingresados incorrectamente"
 exit 1
-else
-echo "Datos OK"
-exit 0
 fi
 descargar(){ # Recibe la URL y devuelve el archivo img.jpg
 wget -O img.jpg "$1"
 }
 cotejar(){ # Recibe la suma ingresada por el usuario y la del archivo .zip
 [ $1 =~ $2 ] && exit 0
-[ ! $1 =~ $2 ] && exit 1
+[ ! $1 =~ $2 ] && exit 2
 }
 SUMA=$"sha256sum $URL"
+cotejar "$SUM" "$SUMA" && descargar "$URL"
