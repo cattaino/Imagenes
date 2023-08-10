7e4cdba5c4bae96ee34ff1a617712495bc0aa94b
cattaino <dantecattaino@gmail.com>
2023-08-10 19:38:19 +0000

    Script 'descargar.sh' - 4th approach

--- descargar.sh
+++ descargar.sh
@@ -6,18 +6,31 @@ if [[ ! $URL =~ $HTPS || ! $# -eq 2 ]]; then
 echo "Datos ingresados incorrectamente"
 exit 1
 fi
-descargar(){ # Recibe la URL y devuelve el archivo img.jpg
-wget -O img.jpg "$1"
+descargar(){ # Recibe la URL y devuelve el archivo .jpg
+wget "$1"
 }
 cotejar(){ # Recibe la suma ingresada por el usuario y la del archivo .zip
-[ $1 =~ $2 ] && exit 0
-[ ! $1 =~ $2 ] && exit 2
+if [[ $1 =~ $2 ]]; then
+exit 0
+else
+exit 2
+fi
 }
-SUMA=$"sha256sum $URL"
-# No aplicar sha256sum a $URL sino al archivo una vez descargado
-#cotejar "$SUM" "$SUMA" && descargar "$URL"
-if cotejar "$SUM" "$SUMA"; then
+obtener_suma(){ # Recibe un archivo y devuelve su suma de verificacion
+sha256sum "$1"
+}
+nombre_archivo(){ # Recibe la URL de un archivo y devuelve su nombre
+echo $1 | tr "/" "\n" | tail -n 1
+}
 descargar "$URL"
+IMG=nombre_archivo "$URL"
+# (linea anterior) https://github.com/cattaino/Imagenes
+# /blob/imagenes/cattaino.jpg: No such file or directory
+# (linea siguiente) command not found
+SUMA=obtener_suma "$IMG"
+if cotejar "$SUM" "$SUMA"; then
+echo "Operacion terminada correctamente"
 else
 echo "Suma de verificacion incorrecta"
+rm $"nombre_archivo $URL"
 fi
