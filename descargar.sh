feeb66d5a6b6a1c7b994cdf4407cdd2b642a0086
cattaino <dantecattaino@gmail.com>
2023-08-11 14:41:56 +0000

    Script 'descargar.sh' - 6 th approach

--- descargar.sh
+++ descargar.sh
@@ -10,7 +10,7 @@ descargar(){ # Recibe la URL y devuelve el archivo .jpg
 wget "$1"
 }
 cotejar(){ # Recibe la suma ingresada por el usuario y la del archivo .zip
-if [[ $1 =~ $2 ]]; then
+if [ "$1" = "$2" ]; then
 exit 0
 else
 exit 2
@@ -20,17 +20,14 @@ obtener_suma(){ # Recibe un archivo y devuelve su suma de verificacion
 sha256sum "$1"
 }
 nombre_archivo(){ # Recibe la URL de un archivo y devuelve su nombre
-echo $1 | tr "/" "\n" | tail -n 1
+echo "$1" | tr "/" "\n" | tail -n 1
 }
 descargar "$URL"
 IMG="nombre_archivo $URL"
-# (linea anterior) https://github.com/cattaino/Imagenes
-# /blob/imagenes/cattaino.jpg: No such file or directory
-# (linea siguiente) command not found
 SUMA="obtener_suma $IMG"
-if cotejar "$SUM" "$SUMA"; then
+cotejar "$SUM" "$SUMA"
+if [ $? -eq 0 ]; then
 echo "Operacion terminada correctamente"
 else
-echo "Suma de verificacion incorrecta"
-rm $"nombre_archivo $URL"
+echo "Suma de verificacion incorrecta" && rm $"nombre_archivo $URL"
 fi
